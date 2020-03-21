5d6049e52a9414c45358cf55ac7d395b1cf3003d
Steven Craig Moss <sc.moss@outlook.com>
2020-03-21 00:44:24 -0400

    attempt 1

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,6 +1,35 @@
 // Step 2: Create Tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
+const newTab = (tablink) => {
+    const childTab = document.createElement('div');
+    const tabEntryPoint = document.querySelector('.tabs .topics')
+    childTab.classList.add('tab');
+    childTab.textContent = tablink
+    tabEntryPoint.appendChild(childTab)
+    return childTab;
+}
+
+
+const getData = () => {
+    axios.get('https://lambda-times-backend.herokuapp.com/topics')
+        .then(response => {
+            console.log(response)
+            const topics = response.data.topics
+            console.log(topics)
+            topics.forEach(topic => {
+                newTab(topic)
+            })
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+getData();
+
+
+
+
 // Once the data is returned console.log it and review the structure.
 // Iterate over the topics creating a new Tab component and add it to the DOM
 // under the .topics element.
