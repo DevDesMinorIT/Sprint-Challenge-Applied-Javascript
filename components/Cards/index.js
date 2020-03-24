74033dec7f092eb39da6901d983494fafaf56e3c
Steven Craig Moss <sc.moss@outlook.com>
2020-03-23 21:42:23 -0400

    i have finished

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -11,23 +11,44 @@
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
     .then(response => {
         const cards = document.querySelector('.cards-container')
-        cards.appendChild(cardComponent(response.data))
-        console.log('this is data', response.data)
-        console.log('you have won', response.data.articles.javascript['headline'])
+        //cards.appendChild(cardComponent(response.data))
+        console.log('this is data', response.data.articles)
+        const newArray = [];
+        const dataArray = response.data.articles;
+
+        dataArray.javascript.forEach(item => newArray.push(item))
+        dataArray.bootstrap.forEach(item => newArray.push(item))
+        dataArray.technology.forEach(item => newArray.push(item))
+        dataArray.jquery.forEach(item => newArray.push(item))
+        dataArray.node.forEach(item => newArray.push(item))
+
+        function createComponent(item) {
+            let
+        }
+        newArray.forEach(item => {
+            let card = cardComponent(item);
+            // headline.textContent = item.headline;
+            // authorImage.src = item.authorImage;
+            // span.textContent = `By: ${item.authorName}`;
+            const newCard = document.querySelector('.cards-container');
+            newCard.appendChild(card);
+        })
+        console.log(newArray)
+        // console.log('you have won', response.data.articles.javascript['headline'])
     })
     .catch(error => {
         console.log(error)
     })
 
-getData3()
-const cardComponent = (banana) => {
+// getData3()
+const cardComponent = (item) => {
     const card = document.createElement('div');
     const headline = document.createElement('div');
     const author = document.createElement('div');
     const imgContainer = document.createElement('div');
     const authorImage = document.createElement('img');
     const span = document.createElement('span');
-
+    // console.log(item.authorImage)
     card.appendChild(headline);
     card.appendChild(author);
     author.appendChild(imgContainer);
@@ -39,10 +60,11 @@ const cardComponent = (banana) => {
     author.classList.add('author');
     imgContainer.classList.add('img-container');
 
+    headline.textContent = item.headline;
+    authorImage.src = item.authorPhoto;
+    span.textContent = `By: ${item.authorName}`;
     // make and array of topics AT TOP OF cardContainer with forEach
-    headline.textContent = data.topics.Array['headline']
-        authorImage.src =
-        span.textContent = "By: "
+
     return card
 }
 // <div class="card">
